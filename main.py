2a82a8cdc27a156a4ad224d5fc00ed99e8b5f5ec
Ankur <34418414+Ankuraxz@users.noreply.github.com>
2020-11-20 16:31:09 +0530

    add arr support

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ model = joblib.load("modelnew.pkl")
 
 @app.get('/')
 def index():
-    return {'message': 'Hello world_____API TESTER'}
+    return {'message': 'Hello world_____API TESTER!!! Use /predict/ to get endpoint and pass 50 answeres in format of arr[1,2,3...] or srting 1234'}
 
 @app.get('/predict/{data}')
 def predict_cluster(data):
@@ -19,16 +19,19 @@ def predict_cluster(data):
         dfx=[]
         for ix in data:
             dfx.append(int(ix))
-        print(dfx)
+#         print(dfx)
         df = pd.DataFrame(dfx)
-
         df = df.T
-
-
         cluster = model.predict(df)
         return(int(cluster))
-    else:
-        return("PASS STRING") #-->testing
+    
+    
+    elif type(data)==list:
+        df = pd.DataFrame(data)
+        df = df.T
+        cluster = model.predict(df)
+        
+        return(int(cluster)) #-->testing
 
 
 # if __name__ == '__main__':
